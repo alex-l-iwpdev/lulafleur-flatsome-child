f8466872cf334d161a693116bb727201b9932fff
Alex Lavigin <153559840+alex-l-iwpdev@users.noreply.github.com>
2025-03-02 13:27:38 +0100

    add Checkout fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 /* global jQuery */
 
 jQuery( document ).ready( function( $ ) {
-	$( '#unknown_address' ).change( function() {
-		$( '#receiver_phone_field' ).toggle( this.checked );
-	} );
+	const unknownAddress = $( '#unknown_address' );
+	if ( unknownAddress.length ) {
+		unknownAddress.change( function() {
+			$( '#receiver_phone_field' ).toggle( this.checked );
+		} );
+	}
+
+	const notVatInvoice = $( '#not_vat' );
+	if ( notVatInvoice.length ) {
+		notVatInvoice.change( function() {
+			$( '#company_name_field' ).toggle( this.checked );
+			$( '#company_address_field' ).toggle( this.checked );
+			$( '#company_nip_field' ).toggle( this.checked );
+		} );
+	}
 } );
--- src/php/WooCommerce/Checkout.php
+++ src/php/WooCommerce/Checkout.php
@@ -8,6 +8,7 @@
 namespace Iwpdev\FlatsomeChild\WooCommerce;
 
 use WC_Checkout;
+use WC_Order;
 
 /**
  * Checkout class file.
@@ -27,8 +28,71 @@ class Checkout {
 	 */
 	private function init(): void {
 		add_action( 'woocommerce_after_checkout_billing_form', [ $this, 'add_unknown_address_checkbox' ] );
-
+		add_action( 'woocommerce_after_checkout_billing_form', [ $this, 'add_not_vat_invoice' ] );
 		add_action( 'woocommerce_checkout_update_order_meta', [ $this, 'save_date_unknown_address' ] );
+		add_action( 'woocommerce_checkout_update_order_meta', [ $this, 'save_date_not_vat_invoice' ] );
+		add_action( 'woocommerce_admin_order_data_after_billing_address', [ $this, 'show_unknown_address_in_order' ] );
+		add_action( 'woocommerce_admin_order_data_after_billing_address', [ $this, 'show_not_vat_in_order' ] );
+	}
+
+	/**
+	 * Add Not VAT Invoice.
+	 *
+	 * @param WC_Checkout $checkout Woocommerce Checkout class.
+	 *
+	 * @return void
+	 */
+	public function add_not_vat_invoice( WC_Checkout $checkout ): void {
+		echo '<div id="not_vat_checkbox">';
+		woocommerce_form_field(
+			'not_vat',
+			[
+				'type'  => 'checkbox',
+				'class' => [ 'form-row-wide' ],
+				'label' => __( 'Invoice (not VAT)', 'flatsome' ),
+			],
+			$checkout->get_value( 'not_vat' )
+		);
+
+		echo '<div id="company_name_field" style="display:none;">';
+		woocommerce_form_field(
+			'company_name',
+			[
+				'type'        => 'text',
+				'class'       => [ 'form-row-wide' ],
+				'label'       => __( 'Company Name', 'flatsome' ),
+				'required'    => true,
+				'placeholder' => __( 'Enter your company name', 'flatsome' ),
+			],
+			$checkout->get_value( 'company_name' )
+		);
+		echo '</div>';
+		echo '<div id="company_address_field" style="display:none;">';
+		woocommerce_form_field(
+			'company_address',
+			[
+				'type'        => 'text',
+				'class'       => [ 'form-row-wide' ],
+				'label'       => __( 'Company Address', 'flatsome' ),
+				'required'    => true,
+				'placeholder' => __( 'Enter your company address', 'flatsome' ),
+			],
+			$checkout->get_value( 'company_address' )
+		);
+		echo '</div>';
+		echo '<div id="company_nip_field" style="display:none;">';
+		woocommerce_form_field(
+			'company_nip',
+			[
+				'type'        => 'text',
+				'class'       => [ 'form-row-wide' ],
+				'label'       => __( 'Company NIP', 'flatsome' ),
+				'required'    => true,
+				'placeholder' => __( 'Enter your company NIP', 'flatsome' ),
+			],
+			$checkout->get_value( 'company_nip' )
+		);
+		echo '</div></div>';
 	}
 
 	/**
@@ -66,6 +130,29 @@ class Checkout {
 	}
 
 	/**
+	 * Save Data Not VAT Invoice.
+	 *
+	 * @param int $order_id Order ID.
+	 *
+	 * @return void
+	 */
+	public function save_date_not_vat_invoice( int $order_id ): void {
+		update_post_meta( $order_id, '_not_vat', ! empty( $_POST['not_vat'] ) ? 'yes' : 'no' );
+
+		if ( ! empty( $_POST['company_name'] ) ) {
+			update_post_meta( $order_id, '_company_name', sanitize_text_field( wp_unslash( $_POST['company_name'] ) ) );
+		}
+
+		if ( ! empty( $_POST['company_address'] ) ) {
+			update_post_meta( $order_id, '_company_address', sanitize_text_field( wp_unslash( $_POST['company_address'] ) ) );
+		}
+
+		if ( ! empty( $_POST['company_nip'] ) ) {
+			update_post_meta( $order_id, '_company_nip', sanitize_text_field( wp_unslash( $_POST['company_nip'] ) ) );
+		}
+	}
+
+	/**
 	 * Save Data Unknown Address.
 	 *
 	 * @param int $order_id Order ID.
@@ -77,6 +164,54 @@ class Checkout {
 
 		if ( ! empty( $_POST['receiver_phone'] ) ) {
 			update_post_meta( $order_id, '_receiver_phone', sanitize_text_field( wp_unslash( $_POST['receiver_phone'] ) ) );
+		}
+	}
+
+	/**
+	 * Show Not VAT in Order.
+	 *
+	 * @param WC_Order $order WooCommerce Order class.
+	 *
+	 * @return void
+	 */
+	public function show_not_vat_in_order( WC_Order $order ): void {
+		$not_vat         = get_post_meta( $order->get_id(), '_not_vat', true );
+		$company_name    = get_post_meta( $order->get_id(), '_company_name', true );
+		$company_address = get_post_meta( $order->get_id(), '_company_address', true );
+		$company_nip     = get_post_meta( $order->get_id(), '_company_nip', true );
+
+		if ( $not_vat === 'yes' ) {
+			echo '<p><strong>' . __( 'Invoice (not VAT)', 'flatsome' ) . ':</strong> ✔</p>';
+			if ( $company_name ) {
+				echo '<p><strong>' . __( 'Company Name', 'flatsome' ) . ':</strong> ' . esc_html( $company_name ) . '</p>';
+			}
+
+			if ( $company_address ) {
+				echo '<p><strong>' . __( 'Company Address', 'flatsome' ) . ':</strong> ' . esc_html( $company_address ) . '</p>';
+			}
+
+			if ( $company_nip ) {
+				echo '<p><strong>' . __( 'Company NIP', 'flatsome' ) . ':</strong> ' . esc_html( $company_nip ) . '</p>';
+			}
+		}
+	}
+
+	/**
+	 * Show Unknown Address in Order.
+	 *
+	 * @param WC_Order $order WooCommerce Order class.
+	 *
+	 * @return void
+	 */
+	public function show_unknown_address_in_order( WC_Order $order ): void {
+		$unknown_address = get_post_meta( $order->get_id(), '_unknown_address', true );
+		$receiver_phone  = get_post_meta( $order->get_id(), '_receiver_phone', true );
+
+		if ( $unknown_address === 'yes' ) {
+			echo '<p><strong>' . __( 'I don\'t know the recipient\'s address', 'flatsome' ) . ':</strong> ✔</p>';
+			if ( $receiver_phone ) {
+				echo '<p><strong>' . __( 'Phone of the recipient', 'flatsome' ) . ':</strong> ' . esc_html( $receiver_phone ) . '</p>';
+			}
 		}
 	}
 }
